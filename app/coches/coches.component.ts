a07705589736e81417f6ca49c667d49406cacd1e
José Pedro Montero González <pedro@d80.mx>
2017-06-28 17:18:08 -0500

    avances

--- app/coches/coches.component.ts
+++ app/coches/coches.component.ts
@@ -8,13 +8,19 @@ import {Coche} from './coche';
 
 export class CochesComponent {
 	public coche:Coche;
-
+	public coches:Array<Coche>;
 	constructor() {
 		this.coche = new Coche("","","");
+		this.coches = [
+			new Coche("Civic SI","210","Rojo"),
+			new Coche("GSR 200","190","Tinto")
+		];
 	}
 
 	onSubmit(){
 		console.log(this.coche);
+		this.coches.push(this.coche);
+		this.coche = new Coche("","","");
 	}
 
 }
