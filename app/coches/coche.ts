3d212be554b5e4ede7ff7c879bbb062693d3502c
JPedroMG <axis31@gmail.com>
2017-09-15 17:25:42 -0500

    webapp prmer commit

--- app/coches/coche.ts
+++
@@ -1,7 +0,0 @@
-export class Coche{ //esto es un modelo que se necesita para trabajar con un formulario
-	constructor(
-		public nombre:string,
-		public hp:string,
-		public color:string
-	){}
-}
--- app/components/producto-add.component.ts
+++ app/components/producto-add.component.ts
@@ -2,6 +2,8 @@ import {Component} from '@angular/core';
 import {Router, ActivatedRoute, Params} from '@angular/router';
 import {ProductoService} from '../services/producto.service';
 import {Producto} from '../models/producto';
+import {GLOBAL} from '../Services/global';
+
 
 @Component({
 	selector:'producto-add',
@@ -12,6 +14,8 @@ import {Producto} from '../models/producto';
 export class ProductoAddComponent{
 	public titulo:string;
 	public producto:Producto;
+	public filesToUpload;
+	public resultUpload;
 
 	constructor(
 		private _productoService:ProductoService,
@@ -28,10 +32,23 @@ export class ProductoAddComponent{
 
 	onSubmit(){
 		console.log(this.producto);
+
+		this._productoService.makeFileRequest(GLOBAL.url+'upload-file',[],this.filesToUpload).then(
+		(result)=>{
+			console.log("exito");
+			console.log(result);
+			this.producto.imagen="ruta";
+		},
+		(error)=>{
+			///console.log(<any>error);
+			console.log("error");
+		}
+		);
+
 		this._productoService.addProducto(this.producto).subscribe(
 			result=>{
-				if(result.code==200){
-					this._router.navigate(['/home']);
+				if(result.codigo==200){
+					this._router.navigate(['/productos']);
 				}else{
 					console.log(result)
 				}
@@ -40,5 +57,10 @@ export class ProductoAddComponent{
 				console.log(<any>error);
 			}
 		);
+	}
+
+	fileChangeEvent(fileInput:any){
+		this.filesToUpload=<Array<File>>fileInput.target.files;
+		console.log(this.filesToUpload);
 	}
 }
